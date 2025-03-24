7ec04729c608591f13ce566c453d21a906d11a88
Denis <dryco@gmail.com>
2025-03-25 00:27:09 +0500

    Изменение конфигурации базы данных с удалением строки подключения к облачному MongoDB на локальный экземпляр

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'database': 'mongodb+srv://Drykov:WfnunIK9a3m792Z5@cluster0.c48ewsz.mongodb.net/radius'
+    'database': 'mongodb://localhost:27017/radius'
 };
 
 // const uri = "mongodb+srv://Drykov:WfnunIK9a3m792Z5@cluster0.c48ewsz.mongodb.net/?appName=Cluster0";
